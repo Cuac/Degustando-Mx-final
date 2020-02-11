29dfd2cb4416a164943fa5f21755d80117d5deb5
AlexQ28 <aquezada010@gmail.com>
2020-02-10 22:02:41 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Proyecto Degustando
         </a>
       </header>
     </div>
