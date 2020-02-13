ca9344c3ce214384f12f2e66287aeae2e75ded8c
AlexQ28 <aquezada010@gmail.com>
2020-02-12 21:37:19 -0600

    cambios 21

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import './assets/css/App.css'; //Importando estilos 
-<<<<<<< HEAD
 import './assets/css/login.css'
 import Inicio from './pages/inicio';
 import Login from './pages/login'
 import Negocios from './pages/negocios';
-
-=======
 import './assets/css/inicio.css';
-
-
-//Importando el componente Inicio
-import Inicio from './pages/inicio';
-import Calificar from "./pages/calificar";
->>>>>>> origin/calificar
 
 function App () {
     return (
@@ -22,15 +13,11 @@ function App () {
       <BrowserRouter>
         <Switch>
            <Route exact path="/" component={Inicio}/>
-<<<<<<< HEAD
            <Route path="/Login" component={Login}/>
            <Route path="/Negocios" component={Negocios}/>
-           
-=======
-           <Route path="/" component={Calificar}/>
-
->>>>>>> origin/calificar
+          
         </Switch>
+
       </BrowserRouter>
   );
 }
