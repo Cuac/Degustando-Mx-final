cb4523b4be4533073100ea01b43ae82db1b6b74a
denissesantiago <denisse.santi4@gmail.com>
2020-02-13 04:19:18 +0100

    mis cambios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import './assets/css/App.css'; //Importando estilos 
-<<<<<<< HEAD
-import './assets/css/Inicio.css';
-=======
->>>>>>> origin/inicio
 
 import './assets/css/login.css'
 import Inicio from './pages/inicio';
-<<<<<<< HEAD
-import Negocios from './pages/negocios';
-=======
 import Login from './pages/login'
 import Negocios from './pages/negocios';
 
->>>>>>> origin/inicio
 
 function App () {
     return (
@@ -23,14 +15,11 @@ function App () {
         <Switch>
 
            <Route exact path="/" component={Inicio}/>
-<<<<<<< HEAD
-=======
         
            <Route path="/Login" component={Login}/>
         
            <Route path="/Negocios" component={Negocios}/>
 
->>>>>>> origin/inicio
 
         </Switch>
       </BrowserRouter>
