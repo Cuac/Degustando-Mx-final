c966e582c24b5a880f845dbf6667c85aa1dce43c
BatmanB|1173 <patricio.medes@hotmail.com>
2020-02-12 19:54:29 -0600

    mas cambios

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-import Login from './pages/login'
-
-import './pages/login.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Proyecto Degustando
-        </a>
-      </header>
-    </div>
-=======
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import './assets/scss/App.scss'; //Importando estilos 
-import './assets/scss/Inicio.scss';
-
+//import './assets/scss/App.scss'; //Importando estilos 
+//import './assets/scss/Inicio.scss';
+import Inicio from './pages/inicio';
+import Login from './pages/login'
 //Importando el componente Inicio
-import Inicio from './pages/Inicio';
+
+
 
 function App () {
     return (
@@ -37,10 +15,11 @@ function App () {
         <Switch>
 
            <Route path="/Inicio" component={Inicio}/>
+           <Route path="/Inicio" component={Login}/>
+
 
         </Switch>
       </BrowserRouter>
->>>>>>> origin/inicio
   );
 }
 
