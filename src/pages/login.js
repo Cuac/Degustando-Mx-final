d04474d9a35116400c22df52533b68bda451b158
BatmanB|1173 <patricio.medes@hotmail.com>
2020-02-12 20:49:08 -0600

    Se linkea login con inicio

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,29 @@
 import React from "react";
-import './assets/css/login.css'
+import '../assets/css/login.css';
+import appLogo from '../assets/images/degus.png';
+import { Link} from 'react-router-dom';
+
+
+
+
 function Login() {
     return (
     
     <div className='centro'>
-        <h1 className='titulo'>Regstro</h1>
-        <input className='registros'placeholder= 'Correo electronico' />
+        <h1 className='titulo'>Registro</h1>
+        <div className="flex-logo" >
+         <div> 
+             <img src={appLogo}  alt="Logo"></img>  
+          </div>
+        </div>  
+        <input className='registros' placeholder= 'Correo electronico' />
+        <input className='registros' placeholder= 'Confirmar correo electronico'/>
+        <input className='registros' placeholder= 'Confirmar contraceña' />
         <input className='registros' placeholder= 'Contraceña' />
+        <input className='check' type='checkbox' placeholder= "Recordarme" />
+        <Link to='/inicio'> Registrarme  </Link>
+
+        
     </div>
     )
 
