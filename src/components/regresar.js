16855138abca1d3f536a0b37645fdc798fccb0fa
denissesantiago <denisse.santi4@gmail.com>
2020-02-13 04:37:01 +0100

    flecha

--- src/components/regresar.js
+++ src/components/regresar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import './regresar.css'
 
 
 function Regresar () {
     return (
-        <div>
+        <div className="navbar-arrow">
             <FontAwesomeIcon icon={faArrowAltCircleLeft} />
         </div>
     );
