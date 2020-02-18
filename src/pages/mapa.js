b15a5ecc5483b46849a6c47aca11372616364da4
BatmanB|1173 <patricio.medes@hotmail.com>
2020-02-17 22:00:11 -0600

    cambios a mapa js por primera vez

--- src/pages/mapa.js
+++ src/pages/mapa.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link} from 'react-router-dom';
 
  function  Mapa () {
        return (
-         
-           <div>
-             <input />
-               <p>Busqueda</p>
-             
-           </div>
-
+         <div>
+            <navigator>
+           <header>
+              <input className='registros' placeholder= 'Buscar garnacha' />
+           </header>
+           <Link  className='boton' to=''> Mi perfil  </Link>
+          </navigator>
+        </div>
        );
     }
 
