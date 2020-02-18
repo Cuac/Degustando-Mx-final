1fac3be5b326cb310cffb2a4aba4d07135060027
denissesantiago <denisse.santi4@gmail.com>
2020-02-18 05:04:27 +0100

    prueba de componente

--- src/pages/negocios.js
+++ src/pages/negocios.js
@@ -5,6 +5,8 @@ import BarraImgNegocios from '../components/barra-img-negocios';
 import InfoNegocios from '../components/info-negocios';
 import OpinionsUsers from '../components/opinions-users';
 import MenuNegocios from "../components/menu-negocios";
+import MyMapComponent from "../components/ubicacion";
+
 
 
 function Negocios() {
@@ -15,6 +17,15 @@ function Negocios() {
             <InfoNegocios/> 
             <OpinionsUsers />
             <MenuNegocios />
+            /* componente mapa de prueba */
+            <MyMapComponent
+            isMarkerShown
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            />
+            
         </div>
 
     )
