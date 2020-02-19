43644650f58ffe08019146ee4d17e761994c1ea2
Elle <elle@iMac-de-Elle.local>
2020-02-19 17:46:09 -0600

    cambios denisse vista negocios

--- src/components/opinions-users.js
+++ src/components/opinions-users.js
@@ -1,34 +1,33 @@
 import React from "react";
-import './opinions-users.css';
+import "./opinions-users.css";
 
 import userAvatar1 from "../assets/images/user-1.png";
 import userAvatar2 from "../assets/images/user-2.png";
 
-function OpinionsUsers () {
-    return (
-        <section>
-            <div className="opiniones-users">
-                <div className="user-description"> 
-                    <div>
-                        <img src={userAvatar1} alt="avatarUsuario1" />
-                    </div>
-                    <div>
-                        <p>Limpio y buen servicio, nos atienden rápido siempre</p>
-                    </div>
-                </div>  
-            
-                <div className="user-description" > 
-                    <div>
-                        <img src={userAvatar2} alt="avatarUsuario2" />
-                    </div>
-                    <div>
-                        <p>Buen sabor y buna variedad de salsitas ¡Recomendado!</p>
-                    </div>
-                </div> 
-            </div>
-        </section>
-    );
-  }
+function OpinionsUsers() {
+  return (
+    <section>
+      <div className="opiniones-users">
+        <div className="user-description">
+          <div>
+            <img src={userAvatar1} alt="avatarUsuario1" />
+          </div>
+          <div>
+            <p>Limpio y buen servicio, nos atienden rápido siempre.</p>
+          </div>
+        </div>
 
+        <div className="user-description" id="user-2">
+          <div>
+            <img src={userAvatar2} alt="avatarUsuario2" />
+          </div>
+          <div>
+            <p>Buen sabor y buna variedad de salsitas ¡Recomendado!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
 
-export default OpinionsUsers; 
+export default OpinionsUsers;
--- src/pages/negocios.js
+++ src/pages/negocios.js
@@ -1,34 +1,30 @@
 import React from "react";
 
-import Regresar from '../components/regresar';
-import BarraImgNegocios from '../components/barra-img-negocios';
-import InfoNegocios from '../components/info-negocios';
-import OpinionsUsers from '../components/opinions-users';
+import Regresar from "../components/regresar";
+import BarraImgNegocios from "../components/barra-img-negocios";
+import InfoNegocios from "../components/info-negocios";
+import OpinionsUsers from "../components/opinions-users";
 import MenuNegocios from "../components/menu-negocios";
 import MyMapComponent from "../components/ubicacion";
 
-
-
 function Negocios() {
-    return (
-        <div>
-            <Regresar/>
-            <BarraImgNegocios/>
-            <InfoNegocios/> 
-            <OpinionsUsers />
-            <MenuNegocios />
-            /* componente mapa de prueba */
-            <MyMapComponent
-            isMarkerShown
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-            />
-            
-        </div>
+  return (
+    <div>
+      <Regresar />
+      <BarraImgNegocios />
+      <InfoNegocios />
+      <OpinionsUsers />
+      <MenuNegocios />
 
-    )
+      <MyMapComponent
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
 }
 
 export default Negocios;
