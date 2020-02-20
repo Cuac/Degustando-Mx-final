e1e5ba691c333c4a4837cca54d75ac5ceab549e6
AlexQ28 <aquezada010@gmail.com>
2020-02-19 19:15:10 -0600

    cambios 19 Febrero

--- src/pages/mapa.js
+++ src/pages/mapa.js
@@ -1,16 +1,27 @@
 import React from "react";
+import '../assets/css/mapa.css';
+
 
  function  Mapa () {
-       return (
-         
-           <div>
-             <input />
-               <p>Busqueda</p>
-             
-           </div>
-
-       );
-    }
-
+   return (
+     <div className="flex-inicio" > 
+         <input className="flex-input" placeholder="Buscar ...."   />
+       <div>
+         <i class="fa fa-bars"></i>
+       </div> 
+   
+       <div>
+          <ul>
+            <li>Bebidas</li>
+            <li>Mexicana</li>
+            <li>Vegetariana</li>
+            <li>Postres</li>
+            <li>Desayunos</li>
+            <li>Postres</li>
+            </ul>
+        </div>
+      </div>
+    );
+}
 
 export default Mapa;
