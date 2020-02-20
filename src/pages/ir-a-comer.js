340ab9e5869e4c2fa9b14a835793fc887c7cb034
denissesantiago <denisse.santi4@gmail.com>
2020-02-20 03:59:56 +0100

    agrego logo

--- src/pages/ir-a-comer.js
+++ src/pages/ir-a-comer.js
@@ -1,17 +1,24 @@
 import React from "react";
 import MyMapComponent from "../components/ubicacion";
 import "../assets/css/ir-a-comer.css";
+import Regresar from "../components/regresar";
 
 function IrAComer() {
   return (
-    <div className="mapa-a-comer">
-      <MyMapComponent
-        isMarkerShown
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `90vh`}} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+    <div>
+      <Regresar />
+      <div className="mapa-a-comer">
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `80vh`}} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+      <div className="boton-vamosacomer">
+        <button className="boton"> Aceptar </button>
+      </div>
 
     </div>
   );
