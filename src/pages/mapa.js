f1e8a6fcebf43006ad9356314872601e22679227
AlexQ28 <aquezada010@gmail.com>
2020-02-24 19:32:19 -0600

    cambios 2402

--- src/pages/mapa.js
+++ src/pages/mapa.js
@@ -64,7 +64,7 @@ class Mapa extends React.Component {
                   googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCey8p9i3fkHzj6usQdPXVcAsD3DSNW_Kw&v=3.exp&libraries=geometry,drawing,places"
                   loadingElement={<div style={{ height: `100%` }} />}
                   containerElement={<div style={{ height: `400px` }} />}
-                  mapElement={<div style={{ height: `100%` }} />}
+                  mapElement={<div style={{ height: `100vh` }} />}
                   location={{lat:this.state.latitude, lng: this.state.longitude}}
               />
            </div>
