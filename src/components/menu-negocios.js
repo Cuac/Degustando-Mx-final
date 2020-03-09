79170820e6d5aa0169bb2473f43e4a54f2dce0e1
denissesantiago <denisse.santi4@gmail.com>
2020-03-09 14:54:17 -0600

    media queries denisse negocios

--- src/components/menu-negocios.js
+++ src/components/menu-negocios.js
@@ -1,31 +1,24 @@
 import React from "react";
-import '../assets/css/menu-negocios.css';
+import "../assets/css/menu-negocios.css";
 
-function MenuNegocios () {
-    return (
-        <section>
-            <div className="menu-comida">
-                <h2>Menú</h2>
-                <ul>
-                    <li>
-                        Tacos de pastor con todo -  $13
-                    </li>
-                    <li>
-                        Tacos de suadero con todo - $14
-                    </li>
-                    <li>
-                        Gringa de pastos natural - $40
-                    </li>
-                </ul>
-            </div>
-            <div className="boton-vamosacomer">
-                <button className="boton">
-                    ¡Vamos a comer!
-                </button>
-            </div>
-        </section>
-    );
-  }
+function MenuNegocios() {
+  return (
+    <section>
+      <div className="contenedor-menu">
+        <div className="menu-comida">
+          <h2>Menú</h2>
+          <ul>
+            <li>Tacos de pastor con todo - $13</li>
+            <li>Tacos de suadero con todo - $14</li>
+            <li>Gringa de pastos natural - $40</li>
+          </ul>
+          <div className="boton-vamosacomer">
+            <button className="boton">¡Vamos a comer!</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
 
-
-export default MenuNegocios; 
+export default MenuNegocios;
