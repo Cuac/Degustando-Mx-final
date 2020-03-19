f3013b6380964b0556eeeaecf31250b6cfbab490
Mauricio Perez <ing.mauricioprz@gmail.com>
2020-03-18 21:47:33 -0600

    cambios en calificar

--- src/pages/calificar.js
+++ src/pages/calificar.js
@@ -20,32 +20,27 @@ function Calificar() {
             <img className="imagen-taqueria" src={Taqueria} alt="Taqueria"></img>
             <h3 className="title-taqueria" >Taqueria Don Chucho</h3>
             <p className="review-taqueria">Te recomendamos que pruebes la salsa toreada en tu proxima visita.</p>
-        <div className="qualify" >
-            <h2>Califica</h2>
-                <form>
-                    <p className="clasificacion">
-                        <input id="radio1" type="radio" name="estrellas" value="5"/>
-                        <label for="radio1">★</label>
-                        <input id="radio2" type="radio" name="estrellas" value="4"/>
-                        <label for="radio2">★</label>
-                        <input id="radio3" type="radio" name="estrellas" value="3"/>
-                        <label for="radio3">★</label>
-                        <input id="radio4" type="radio" name="estrellas" value="2"/>
-                        <label for="radio4">★</label>
-                        <input id="radio5" type="radio" name="estrellas" value="1"/>
-                        <label for="radio5">★</label>
-                    </p>
-                    <div>
-                        <textarea className="commit1" rows="5" cols="39">Escribe aquí tus comentarios</textarea>
-                    </div>
-            </form>
-        </div>
-        <div className="boton-qualify">
-        <button className="boton">
-        Enviar
-      </button>
-        </div>
-        
+                
+                    <h2 className="qualify">Califica</h2>
+                    <form className="clasificacion">
+                        <p className="hijo1">
+                            <label for="radio1">★</label>
+                            <input id="radio1" type="radio" name="estrellas" value="5"/>
+                            <label for="radio2">★</label>
+                            <input id="radio2" type="radio" name="estrellas" value="4"/>
+                            <label for="radio3">★</label>
+                            <input id="radio3" type="radio" name="estrellas" value="3"/>
+                            <label for="radio4">★</label>
+                            <input id="radio4" type="radio" name="estrellas" value="2"/>
+                            <label for="radio5">★</label>
+                            <input id="radio5" type="radio" name="estrellas" value="1"/>
+                        </p>
+                        
+                            <textarea className="commit1" rows="5" cols="39">Escribe aquí tus comentarios</textarea>
+                        
+                    </form>
+                    <button className="boton cambios">Enviar</button>
+                 
         </section>
     );
     
